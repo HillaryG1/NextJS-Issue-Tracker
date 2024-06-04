edf451c660dd70aad253f32507716e661cebd877
Hillary X <hillaryx@hillarys-air.mynetworksettings.com>
2024-06-03 22:10:50 -0400

    Add Dynamic Rendering for TicketCard

--- app/(components)/ProgressBar.jsx
+++ app/(components)/ProgressBar.jsx
@@ -1,9 +1,9 @@
-const ProgressBar = () => {
+const ProgressBar = ({ progress }) => {
   return (
     <div className="w-full bg-gray-300 rounded-full h-2.5">
       <div
         className="bg-blue-500 h-2.5 rounded-full"
-        style={{ width: "75%" }}
+        style={{ width: `${progress}%` }}
       ></div>
     </div>
   );
--- app/(components)/StatusDisplay.jsx
+++ app/(components)/StatusDisplay.jsx
@@ -1,7 +1,26 @@
-const StatusDisplay = () => {
+const StatusDisplay = ({ status }) => {
+  const getColor = (status) => {
+    let color = "bg-slate-700";
+    switch (status.toLowerCase()) {
+      case "done":
+        color = "bg-teal-400";
+        return color;
+      case "started":
+        color = "bg-yellow-200";
+        return color;
+      case "not started":
+        color = "bg-red-300";
+        return color;
+    }
+    return color;
+  };
   return (
-    <span className="inline-block rounded-full px-2 py-1 text-xs font-semibold text-gray-700 bg-teal-400">
-      Done
+    <span
+      className={`inline-block rounded-full px-2 py-1 text-xs font-semibold text-gray-700 ${getColor(
+        status
+      )}`}
+    >
+      {status}
     </span>
   );
 };
